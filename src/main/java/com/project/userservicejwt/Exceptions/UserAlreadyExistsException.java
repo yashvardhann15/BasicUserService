e90553fbed38085aed96d35b283098fcd818c2a1
yashvardhann15 <yashvardhangupta318@gmail.com>
2025-05-26 00:15:56 +0530

    initial commit

--- src/main/java/com/project/userservicejwt/Exceptions/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.project.userservicejwt.Exceptions;
-
-public class UserAlreadyExistsException extends RuntimeException{
-    public UserAlreadyExistsException(){
-        super();
-    }
-}
--- src/main/java/com/project/userservicejwt/Exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.project.userservicejwt.Exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException() {
-        super();
-    }
-}
